69183e5c19c1c3ff9e12dd2de6b37295b93b04cd
Kvammeh <charliekvamme@gmail.com>
2021-03-03 23:35:51 -0600

    modified:   commands/update.sh

--- commands/update.sh
+++ commands/update.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
 echo "Updating server."
+
+#sudo systemctl stop valheim
+
+#steamcmd +login anonymous +force_install_dir /home/kvamme/Valheim +app_update 896660 validate +exit
+
+#sudo systemctl start valheim
+
