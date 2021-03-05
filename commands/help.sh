242d7262c6c9ce496c169cd5239dfe24defb2774
Kvammeh <charliekvamme@gmail.com>
2021-03-05 00:52:28 -0600

    new file:   commands/help.sh

---
+++ commands/help.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "!players - lists number of players on running server."
+echo "!ping - shows 1 packet ping information."
+echo "!ip - displays server IP."
+echo "!status - shows status of server service."
+echo "!update - updates game to newest version (testing)"
+echo "!uname"
+echo "!pwd"
