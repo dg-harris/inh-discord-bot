c3d96da1504fa9eb96397f7019c9b2c296fa5c3e
Kvammeh <charliekvamme@gmail.com>
2022-06-25 06:05:13 -0500

    new file:   handlers/vrising.ts 	new file:   shellTasks/vrising-start.sh 	new file:   shellTasks/vrising-status.sh 	new file:   shellTasks/vrising-stop.sh

---
+++ shellTasks/vrising-start.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+(cd ~/VRising && ./start.sh)
+
+echo "V Rising is starting (Give it 5 minutes)."
---
+++ shellTasks/vrising-stop.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+(cd ~/VRising && ./stop.sh)
+echo "V Rising is stopping."
