876931c485b588a2a9e12fd6846b9b4d7d7b7d31
Daniel Harris <15142691+dfloss@users.noreply.github.com>
2022-12-23 21:12:38 -0600

    hack: rename customId to custom_id when sending rest request to discord

--- botResponses/sendInteractionResponse.ts
+++ botResponses/sendInteractionResponse.ts
@@ -1,15 +1,34 @@
+import { DiscordCreateMessagePayload } from "./messaging.types.ts";
+
 interface InteractionContext {
   id: BigInt;
   token: string;
 }
 export const sendInteractionResponse = (
-  response: Record<string, unknown>,
+  response: { type: number; data: DiscordCreateMessagePayload },
   context: InteractionContext
 ) => {
   const url = `https://discord.com/api/v8/interactions/${context.id}/${context.token}/callback`;
+
+  //HACK: fix discordeno payload to match discord payload
+  const components = response.data.components?.map((component) => {
+    const subcomponents = component.components.map((subcomponent) => {
+      const { customId, ...rest } = subcomponent;
+      return {
+        ...rest,
+        custom_id: customId,
+      };
+    });
+    return { ...component, components: subcomponents };
+  });
+
+  const transformedResponse: any = { ...response };
+  transformedResponse.data.components = components;
+  console.log(JSON.stringify(transformedResponse, null, 2));
+
   return fetch(url, {
     method: "POST",
-    body: JSON.stringify(response),
+    body: JSON.stringify(transformedResponse),
     headers: {
       "Content-Type": "application/json",
     },
